47e786f7203d4f42f3cca5e3a6d54ea53d96be41
Ayush07Dec <ayushgautam060@gmail.com>
2026-09-23 21:25:23 +0530

    practice

--- src/main/java/org/example/dsa/stringPractice/FirstNonRepeating.java
+++ src/main/java/org/example/dsa/stringPractice/FirstNonRepeating.java
@@ -7,7 +7,9 @@ public class FirstNonRepeating {
     public static void main(String[] args) {
         String name = "llama";
         System.out.println(nonRepUsingMap(name));
-        System.out.println();
+        System.out.println(repeating(name));
+        System.out.println(repeatingUsingMap(name));
+
 
     }
     static  char nonRepUsingMap(String s){
@@ -27,6 +29,23 @@ public class FirstNonRepeating {
         return '0';
     }
 
+    static  char repeatingUsingMap(String s){
+
+        Map<Character, Integer> frq = new HashMap<>();
+
+        for(int i =0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            frq.put(ch, frq.getOrDefault(ch, 0)+1);
+        }
+
+        for(char c : s.toCharArray()){
+            if(frq.get(c) > 1){
+                return c;
+            }
+        }
+        return '0';
+    }
+
     static char nonRepeating(String s) {
 
         for (int i = 0; i < s.length(); i++) {
@@ -39,6 +58,24 @@ public class FirstNonRepeating {
                 }
             }
             if (!isRepeating) {
+                return ch;
+            }
+        }
+        return '0';
+    }
+
+    static char repeating(String s) {
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            boolean isRepeating = false;
+            for (int j = 0; j < s.length(); j++) {
+                if (i != j && ch == s.charAt(j)) {
+                    isRepeating = true;
+                    break;
+                }
+            }
+            if (isRepeating) {
                 return ch;
             }
         }
